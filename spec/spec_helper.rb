d839ba2de10e4330384064d100ac07562f334b3e
Codemis <johnathan@still-water.com>
2012-01-31 15:38:54 -0800

    Added database cleaner settings.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,4 +29,14 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+  # Clean up the database
+  require 'database_cleaner'
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = "mongoid"
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
 end
