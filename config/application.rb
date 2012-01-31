97594fe1c7733f8d762a53918f166c065b4ad933
Codemis <johnathan@still-water.com>
2012-01-31 15:36:59 -0800

    Supress rspec on views.

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,8 @@ module Cartography
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.generators do |g|
+      g.view_specs false
+    end
   end
 end
