c2d8931475ec27fdc5fd8018e84ccfe4538ce844
Codemis <johnathan@still-water.com>
2012-01-31 15:21:57 -0800

    Adding initial code.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cartography::Application.config.secret_token = '9bac3b48cee6f01862fb497a625c8db5677511396f53e2ce97c64662501fea77cc9ad4a5b2b3f9c2d75cf13f57f0dbc52de46fce7e55e5c1a1f83dfd590c9eb8'
