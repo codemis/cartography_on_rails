3f5425621066c0aa5b5b9584e3808ace572e81e6
Codemis <johnathan@still-water.com>
2012-01-31 16:09:38 -0800

    Added a name field to the user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  field :name
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email, :case_sensitive => false
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
   ## Database authenticatable
   field :email,              :type => String, :null => false, :default => ""
